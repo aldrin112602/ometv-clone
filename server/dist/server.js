7e913a40a4717ff8ea0beb29ac643383fa465514
Aldrin Caballero <caballeroaldrin02@gmail.com>
2026-02-13 19:58:05 +0800

    Updated

--- server/dist/server.js
+++ server/dist/server.js
@@ -30,9 +30,10 @@ function matchUsers(user1, user2) {
     user1.partnerId = user2.id;
     user2.partnerId = user1.id;
     console.log(`Matched ${user1.id} with ${user2.id}`);
-    // Notify both users they've been matched
-    user1.socket.emit('matched', { partnerId: user2.id });
-    user2.socket.emit('matched', { partnerId: user1.id });
+    // User1 (who was waiting) becomes the initiator and creates the offer
+    // User2 (who just requested) will wait for the offer
+    user1.socket.emit('matched', { partnerId: user2.id, initiator: true });
+    user2.socket.emit('matched', { partnerId: user1.id, initiator: false });
 }
 // Find a random partner for a user
 function findPartner(user) {
